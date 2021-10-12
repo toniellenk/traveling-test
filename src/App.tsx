4e230f5ebdf9d47208d39bd43a7c44b89ab2dbe0
Toniel Moraes <toniel.moraes@amaggi.com.br>
2021-10-12 19:25:52 -0400

    - initial commit;

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+
 import './App.css';
+
+import History from './services/History';
+import PublicRoutes from './routes/PublicRoutes';
+import PrivateRoutes from './routes/PrivateRoutes';
+
+
+const authentication = () =>
+  localStorage.getItem('icaseUser') ? <Redirect to="/app" /> : <PublicRoutes />;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Router history={History}>
+        <Switch>
+          <Route path="/app" component={PrivateRoutes} />
+          <Route path="" render={authentication} />
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
---
+++ src/routes/PrivateRoutes.tsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Routers';
+
+// import EsqueciSenha from '~/views/public/EsqueciSenha';
+// import ConfirmacaoEmail from '~/views/public/ConfirmarEmail';
+// import RecuperarSenha from '~/views/public/RecuperarSenha';
+// import DeskAtendimentos from '~/views/public/Desktop/Atendimentos';
+
+const PublicRoutes = () => (
+    <Fragment>
+        <Switch>
+         {/*   <Route path="/" exact component={Login} />
+             <Route path="/esqueci-senha" exact component={EsqueciSenha} />
+      <Route path="/auth/confirmar-email" exact component={ConfirmacaoEmail} />
+      <Route path="/auth/recuperar-senha" exact component={RecuperarSenha} />
+      <Route path="/auth/atendimentos" exact component={DeskAtendimentos} /> */}
+        </Switch>
+    </Fragment>
+);
+
+export default PublicRoutes;
