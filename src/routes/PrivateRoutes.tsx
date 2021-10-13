161b53bd9ca754caef2587d69f62906958e551df
Toniel Moraes <toniel.moraes@amaggi.com.br>
2021-10-13 16:14:59 -0400

    Refac

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,11 +1,5 @@
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import { Switch } from 'react-router-dom';
-import Route from './Routers';
-
-// import EsqueciSenha from '~/views/public/EsqueciSenha';
-// import ConfirmacaoEmail from '~/views/public/ConfirmarEmail';
-// import RecuperarSenha from '~/views/public/RecuperarSenha';
-// import DeskAtendimentos from '~/views/public/Desktop/Atendimentos';
 
 const PrivateRoutes = () => (
     <Fragment>
